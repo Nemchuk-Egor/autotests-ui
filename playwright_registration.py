2ab1693c141e87731e3b8d7a1d82283cbc92b343
Nemchuk-Egor <nemchuk1994@gmail.com>
2026-06-14 22:27:53 +0300

    Маркировки Pytest

--- playwright_registration.py
+++ playwright_registration.py
@@ -5,28 +5,33 @@ with sync_playwright() as playwright:
     context = browser.new_context()
     page = context.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    page.goto(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration"
+    )
 
-    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    registration_email_input = page.get_by_test_id(
+        "registration-form-email-input"
+    ).locator("input")
     expect(registration_email_input).to_be_visible()
     registration_email_input.fill("user.name@gmail.com")
 
-
-    registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    registration_username_input = page.get_by_test_id(
+        "registration-form-username-input"
+    ).locator("input")
     expect(registration_username_input).to_be_visible()
     registration_username_input.fill("username")
 
-
-    registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    registration_password_input = page.get_by_test_id(
+        "registration-form-password-input"
+    ).locator("input")
     expect(registration_password_input).to_be_visible()
     registration_password_input.fill("password")
 
-
-    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button = page.get_by_test_id("registration-page-registration-button")
     expect(registration_button).to_be_visible()
     registration_button.click()
 
-    dashboard_toolbar_title_text = page.get_by_test_id('dashboard-toolbar-title-text')
+    dashboard_toolbar_title_text = page.get_by_test_id("dashboard-toolbar-title-text")
     expect(dashboard_toolbar_title_text).to_be_visible()
     expect(dashboard_toolbar_title_text).to_have_text("Dashboard")
 
@@ -37,4 +42,6 @@ with sync_playwright() as playwright:
     context = browser.new_context(storage_state="browser-state.json")
     page = context.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+    page.goto(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard"
+    )
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,33 +1,46 @@
 from playwright.sync_api import sync_playwright, expect
+import pytest
 
+
+@pytest.mark.regression
+@pytest.mark.registration
 def test_successful_registration():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        page.goto(
+            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration"
+        )
 
-        registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        registration_email_input = page.get_by_test_id(
+            "registration-form-email-input"
+        ).locator("input")
         expect(registration_email_input).to_be_visible()
         registration_email_input.fill("user.name@gmail.com")
 
-
-        registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        registration_username_input = page.get_by_test_id(
+            "registration-form-username-input"
+        ).locator("input")
         expect(registration_username_input).to_be_visible()
         registration_username_input.fill("username")
 
-
-        registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        registration_password_input = page.get_by_test_id(
+            "registration-form-password-input"
+        ).locator("input")
         expect(registration_password_input).to_be_visible()
         registration_password_input.fill("password")
 
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button = page.get_by_test_id(
+            "registration-page-registration-button"
+        )
         expect(registration_button).to_be_visible()
         registration_button.click()
 
-        dashboard_toolbar_title_text = page.get_by_test_id('dashboard-toolbar-title-text')
+        dashboard_toolbar_title_text = page.get_by_test_id(
+            "dashboard-toolbar-title-text"
+        )
         expect(dashboard_toolbar_title_text).to_be_visible()
         expect(dashboard_toolbar_title_text).to_have_text("Dashboard")
 
@@ -38,4 +51,6 @@ def test_successful_registration():
         context = browser.new_context(storage_state="browser-state.json")
         page = context.new_page()
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+        page.goto(
+            "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard"
+        )
