7aee8b5733d164db28cb2149016adf6d135d61d4
Nemchuk-Egor <nemchuk1994@gmail.com>
2026-06-19 01:51:29 +0300

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -7,6 +7,7 @@ from pages.registration_page import RegistrationPage
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
 
+
 @pytest.fixture
 def login_page(chromium_page: Page) -> LoginPage:
     return LoginPage(page=chromium_page)
@@ -21,9 +22,11 @@ def registration_page(chromium_page: Page) -> RegistrationPage:
 def dashboard_page(chromium_page: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page)
 
+
 @pytest.fixture
 def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
     return CoursesListPage(page=chromium_page_with_state)
+
 
 @pytest.fixture
 def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,35 +1,30 @@
-from playwright.sync_api import expect, sync_playwright, Page
 import pytest
+from pages.create_course_page import CreateCoursePage
+from pages.courses_list_page import CoursesListPage
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state: Page):
-    chromium_page_with_state.goto(
-        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses"
+def test_empty_courses_list(
+    create_course_page: CreateCoursePage, courses_list_page: CoursesListPage
+):
+    create_course_page.visit(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create"
     )
-
-    h6_courses_toolbar_title = chromium_page_with_state.get_by_test_id(
-        "courses-list-toolbar-title-text"
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_create_course_form("", "", "", "0", "0")
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.upload_preview_image("./testdata/files/image.png")
+    create_course_page.check_visible_image_upload_view(True)
+    create_course_page.fill_create_course_form(
+        "Playwright", "2 weeks", "Playwright", "100", "10"
     )
-    expect(h6_courses_toolbar_title).to_be_visible()
-    expect(h6_courses_toolbar_title).to_have_text("Courses")
-
-    icon_courses_empty = chromium_page_with_state.get_by_test_id(
-        "courses-list-empty-view-icon"
-    )
-    expect(icon_courses_empty).to_be_visible()
-
-    courses_empty_title_text = chromium_page_with_state.get_by_test_id(
-        "courses-list-empty-view-title-text"
-    )
-    expect(courses_empty_title_text).to_be_visible()
-    expect(courses_empty_title_text).to_have_text("There is no results")
-
-    courses_empty_description_text = chromium_page_with_state.get_by_test_id(
-        "courses-list-empty-view-description-text"
-    )
-    expect(courses_empty_description_text).to_be_visible()
-    expect(courses_empty_description_text).to_have_text(
-        "Results from the load test pipeline will be displayed here"
-    )
+    create_course_page.click_create_course_button()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(0, "Playwright", "100", "10", "2 weeks")
