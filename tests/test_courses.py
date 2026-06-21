cc6d652ea68286a5a66b44c01e0215dd05ff3d50
Nemchuk-Egor <nemchuk1994@gmail.com>
2026-06-22 01:12:14 +0300

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -12,8 +12,7 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
 
     courses_list_page.navbar.check_visible("username")
     courses_list_page.sidebar.check_visible()
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.check_visible_empty_view()
 
 
@@ -27,18 +26,18 @@ def test_create_course(
     )
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form("", "", "", "0", "0")
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
-    create_course_page.upload_preview_image("./testdata/files/image.png")
-    create_course_page.check_visible_image_upload_view(True)
+    create_course_page.image_upload_widget.upload_preview_image(
+        "./testdata/files/image.png"
+    )
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(
         "Playwright", "2 weeks", "Playwright", "100", "10"
     )
     create_course_page.click_create_course_button()
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(0, "Playwright", "100", "10", "2 weeks")
+    courses_list_page.toolbar_view.check_visible()
+    courses_list_page.course_view.check_visible(0, "Playwright", "100", "10", "2 weeks")
