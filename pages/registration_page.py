2426ea0da9403157aa1ee5417b9f9c05997aad8f
Nemchuk-Egor <nemchuk1994@gmail.com>
2026-06-24 23:37:10 +0300

    Знакомство с PageFactory. Практика работы с Page Factory

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,8 +1,12 @@
-from pages.base_page import BasePage
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
+
 from components.authentication.registration_form_component import (
     RegistrationFormComponent,
 )
+from elements.button import Button
+from elements.link import Link
+from elements.text import Text
+from pages.base_page import BasePage
 
 
 class RegistrationPage(BasePage):
@@ -10,24 +14,24 @@ class RegistrationPage(BasePage):
         super().__init__(page)
 
         self.registration_form = RegistrationFormComponent(page)
-        self.authentication_course_title = page.get_by_test_id(
-            "authentication-ui-course-title-text"
+        self.authentication_course_title = Text(
+            page, "authentication-ui-course-title-text", "UI Course"
         )
-        self.login_link = page.get_by_test_id("registration-page-login-link")
-        self.registration_button = page.get_by_test_id(
-            "registration-page-registration-button"
+        self.login_link = Link(page, "registration-page-login-link", "Login")
+        self.registration_button = Button(
+            page, "registration-page-registration-button", "Registration"
         )
-        self.user_again_exists_alert = page.get_by_test_id(
-            "registration-page-user-already-exists-alert"
+        self.user_again_exists_alert = Text(
+            page, "registration-page-user-already-exists-alert", "User already exists"
         )
 
     def click_login_link(self):
         self.login_link.click()
 
     def click_registration_button(self):
-        expect(self.registration_button).to_be_visible()
+        self.registration_button.check_visible()
         self.registration_button.click()
 
     def check_visible_user_again_exists_alert(self):
-        expect(self.user_again_exists_alert).to_be_visible()
-        expect(self.user_again_exists_alert).to_have_text("User already exists")
+        self.user_again_exists_alert.check_visible()
+        self.user_again_exists_alert.check_have_text("User already exists")
