3bb24b8807a635c377ab6a4fbde68dbda301890d
Nemchuk-Egor <nemchuk1994@gmail.com>
2026-06-28 23:24:32 +0300

    Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -53,3 +53,43 @@ class TestCourses:
             estimated_time="2 weeks",
         )
 
+    def test_edit_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+        create_course_page.create_course_form.fill(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10",
+        )
+        create_course_page.image_upload_widget.upload_preview_image(
+            "./testdata/files/image.png"
+        )
+        create_course_page.create_course_toolbar.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title="Playwright",
+            max_score="100",
+            min_score="10",
+            estimated_time="2 weeks",
+        )
+        courses_list_page.course_view.menu.click_edit(index=0)
+
+        create_course_page.create_course_form.fill(
+            title="Playwright_new",
+            estimated_time="now",
+            description="Playwright_new",
+            max_score="1000",
+            min_score="100",
+        )
+        create_course_page.create_course_toolbar.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title="Playwright_new",
+            max_score="1000",
+            min_score="100",
+            estimated_time="now",
+        )
+
